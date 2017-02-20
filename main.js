0d836dc876676ee3d08c3431b72723ada57061e4
Melly <1527891@stud.hs-mannheim.de>
2017-02-20 14:44:17 +0100

    Get-Befehle für Student und Avatar-Infos
    schmeißt am Ende noch einen Fehler, -Zwischenpush-

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
+var tokenS = localStorage.getItem('token');
+var token = JSON.parse(tokenS);
+var student = {};
+var avatare = [];
+
 function passwordÄndern() {
   $('#content').load('content.html #password-change-body');
 }
 function profilbildÄndern(){
   $('#content').load('content.html #profilepic-change-body');
 }
+
+function getUserInfo(){
+    //UserInfo in JSON Modell schreiben
+    var getStudentInfo = {
+        "async": false,
+        "url": "http://46.101.214.215:1337/api/V1/student",
+        "method": "GET",
+        "headers": {
+            "authorization":""}}
+
+    var getAvatarInfo = {
+        "async": false,
+        "url": "http://46.101.214.215:1337/api/V1/avatar",
+        "method": "GET",
+        "headers": {
+            "authorization":""}}
+
+    //ändere den token in studentJSON und avatareJSON
+    getStudentInfo.headers.authorization = token.token;
+    getAvatarInfo.headers.authorization = token.token;
+
+    //Befehl um student zu bekommen
+    $.ajax(getStudentInfo).done(function (response) {
+    student=response;
+    });
+
+    //Befehl um avatare zu bekommen
+    $.ajax(getAvatarInfo).done(function (response) {
+    avatare=response;
+    });
+}
+
+
+$(document).ready(function() {
+  getUserInfo();
+});
